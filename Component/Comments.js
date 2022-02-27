e97109dd1ff46ec892379c2f0a387ebd8f11eaf2
aman-anand17 <legendneverretires@gmail.com>
2022-02-27 16:03:25 +0530

    first commit

---
+++ Component/Comments.js
@@ -0,0 +1,60 @@
+import { useState,useContext } from 'react';
+import { View, Text,Pressable, StyleSheet} from 'react-native';
+import { Card, Title, Paragraph } from 'react-native-paper';
+import { TextInput } from 'react-native-paper';
+import { FontAwesome } from '@expo/vector-icons';
+import CommentCard from './CommentCard';
+import { UpdateComment } from '../Controllers/UpdateComment';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { LoginContext } from '../Contexts/LoginContext';
+
+const Comments = (props)=>{
+    const[text,setText]=useState("");
+    const[commentArr,setCommentArr]=useState(props.data.comments);
+    const dataContext = useContext(LoginContext);
+    
+    const addComment = ()=>{
+        const newarr= [...commentArr,text];     
+        setCommentArr(newarr);
+        UpdateComment(props.data.id,text); 
+        setText("");
+    }
+    return(
+        <View style={{borderTopColor:'gray',borderTopWidth:1}}>
+            {
+                dataContext.islogin?
+                <View style={{flex:1,flexDirection:'row',borderWidth:1,borderColor:'gray',marginBottom:2,alignItems:'center',justifyContent:'space-evenly',backgroundColor:"white"}}> 
+                <TextInput style={{width:"90%",margin:3,backgroundColor:'white'}}
+                    label="comment"
+                    placeholder='enter comment'
+                    type="outlined"
+                    value={text}
+                    multiline={true}
+                    onChangeText={text => setText(text)}
+                />
+                <Pressable onPress={addComment} >
+                    <MaterialCommunityIcons name="send-circle-outline" size={32} color="black" />
+                </Pressable>
+            </View>
+
+                :null
+            }
+            
+            {
+                commentArr.map((cm)=>{
+                    return(
+                    <CommentCard key={cm} comment={cm} />
+                    )
+
+                })
+            }
+
+        </View>
+        
+        
+        
+    );
+
+}
+
+export default Comments;
---
+++ Component/ImagePicker.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import {Image, View, Text } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import { storageRef } from '../Controllers/config';
+import { getStorage, ref,uploadBytes,getDownloadURL } from "firebase/storage";
+
+export default function MyImagePicker(props){
+  const imagesRef = ref(storageRef, 'images');
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 4],
+      quality: 1,
+    });
+
+    // console.log(result);
+
+    if (!result.cancelled) {
+      console.log(props.setImgUrl)
+      props.setImgUrl(result.uri);
+      
+    //   setImage(result.uri);
+      // const myref = ref(imagesRef,'images.jpg')
+      // const imgs = await fetch(result.uri);
+      // const bytes = await imgs.blob();
+      // await uploadBytes(myref,bytes);
+                                                              //     getDownloadURL(ref(imagesRef, 'images.jpg'))
+                                                              // .then((url) => {
+                                                              //   // `url` is the download URL for 'images/stars.jpg'
+                                                              //   console.log(url);
+                                                              // })
+                                                              // .catch((error) => {
+                                                              //   // Handle any errors
+                                                              // });
+
+    }
+  };
+
+  return (
+    <View style={{ flex: 1,margin:12, alignItems: 'center', justifyContent: 'center' }}>
+      <Button mode='outlined' style={{width:'100%'}} color='black' onPress={pickImage} >
+        Pick an image
+      </Button>
+     
+      {/* {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />} */}
+    </View>
+  );
+}
